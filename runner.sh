639bc9c6c16fd7c5760fa39acecdb50bb1254662
David Eriksson <david.eriksson@avanza.se>
2025-10-15 10:05:49 +0200

    Update

--- runner.sh
+++ runner.sh
@@ -17,14 +17,23 @@ sudo apt-get install -y wireguard wireguard-tools openssh-client resolvconf
 
 echo "Configuring WireGuard..."
 # Create wireguard config
+<<<<<<< Updated upstream
 echo "$WIREGUARD_CONFIG" > ./wg0.conf
 echo "$SSH_KEY" > ./ssh.pub
 
 sudo chmod 600 ./wg0.conf ./ssh.pub
+=======
+echo "$WIREGUARD_CONFIG" | sudo tee /etc/wireguard/wg0.conf
+echo "$SSH_KEY" | tee ~/ssh.pub
+>>>>>>> Stashed changes
 
 echo "Starting WireGuard..."
 # Start wireguard
 sudo wg-quick up ./wg0.conf
 
 echo "Running SSH script..."
+<<<<<<< Updated upstream
 ssh -o ConnectTimeout=30 -o BatchMode=yes -o StrictHostKeyChecking=accept-new -i ./ssh.pub -p "$SSH_PORT" "$SSH_USER"@"$SSH_HOST" "$SSH_SCRIPT"
+=======
+ssh -o ConnectTimeout=30 -o BatchMode=yes -o StrictHostKeyChecking=accept-new -i ~/ssh.pub -p "$SSH_PORT" "$SSH_USER"@"$SSH_HOST" "$SSH_SCRIPT"
+>>>>>>> Stashed changes
